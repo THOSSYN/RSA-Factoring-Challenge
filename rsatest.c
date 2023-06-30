619f4bd2f582a9f355b23a3460849d3ff2aa2d48
THOSSYN <mtafortunefinder@gmail.com>
2023-06-30 05:11:34 -0700

    Recompiled: rsa executable file

--- rsatest.c
+++ rsatest.c
@@ -4,88 +4,136 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/**
- * IsprimeFact - Checks a divisor primality
- * @num: is the number to be checked
- * Return: True or False
- */
-bool IsprimeFact(long int num)
-{
-	long int i;
+bool IsprimeFact(long int num) {
+    long int i;
 
-	/*Iterate from 2 till number less than one*/
-	for (i = 2; i < num; i++)
-	{
-		if (num % i == 0)
-			return (false);
-	}
-	return (true);
+    for (i = 2; i < num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
 }
 
-/**
- * GetFactors - Factorizes a prime number
- * @num: is the prime number
- * Return: nothing
- */
+long int PollardRho(long int num) {
+    long int x = 2, y = 2, d = 1;
+    long int c;
 
+    srand(1);
+    c = rand() % (num - 1) + 1;
+
+    while (d == 1) {
+        x = (x * x + c) % num;
+        y = (y * y + c) % num;
+        y = (y * y + c) % num;
+        d = abs(x - y) % num;
+    }
+
+    return d;
+}
+
+long int ModularExponentiation(long int base, long int exponent, long int modulus) {
+    long int result = 1;
+    base = base % modulus;
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            result = (result * base) % modulus;
+
+        base = (base * base) % modulus;
+        exponent /= 2;
+    }
+
+    return result;
+}
 void GetFactors(long int num)
 {
-	long int divv;
+    long int divv;
 
-	/*we start using 2 through num = square of divv*/
-	for (divv = 2; divv * divv <= num; divv++)
-	{
-		if (num % divv == 0 && IsprimeFact(divv))
-		{
-			printf("%ld =%ld*%ld\n", num, num / divv, divv);
-		}
-	}
+    while (num % 2 == 0) {
+        printf("%ld=%ld*2\n", num, num / 2);
+        num = num / 2;
+    }
+
+    for (divv = 3; divv * divv <= num; divv += 2) {
+        while (num % divv == 0) {
+            printf("%ld=%ld*%ld\n", num, num / divv, divv);
+            num = num / divv;
+        }
+    }
+
+    if (num > 2) {
+        printf("%ld=%ld*%ld\n", num, num, 1L);
+    }
+}
+/*long int ModularExponentiation(long int base, long int exponent, long int modulus) {
+    long int result = 1;
+    base = base % modulus;
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            result = (result * base) % modulus;
+
+        base = (base * base) % modulus;
+        exponent /= 2;
+    }
+
+    return result;
 }
 
-/**
- *main - get the number per line from the file
- *@argc: is the number of argument
- *@argv: is the array of string passed to the program
- *Return: 0 on success
- */
+void GetFactors(long int num) {
+    printf("%ld =", num);
+
+    while (num % 2 == 0) {
+        printf(" 2");
+        num = num / 2;
+    }
+
+    for (long int divv = 3; divv * divv <= num; divv += 2) {
+        while (num % divv == 0) {
+            printf(" %ld", divv);
+            num = num / divv;
+        }
+    }
+
+    if (num > 2) {
+        printf(" %ld", num);
+    }
+
+    printf("\n");
+}*/
 
 int main(int argc, char **argv)
 {
-	char *lineptr = NULL;
-	size_t nread = 0;
-	int fd = STDIN_FILENO;
-	FILE *fptr;
+    char *lineptr = NULL;
+    size_t bufsize = 0;
+    ssize_t nread;
+    int fd = STDIN_FILENO;
+    FILE *fptr;
 
-	if (argc > 1)
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-		{
-			fprintf(stderr, "Usage: %s <file>\n", argv[1]);
-			exit(EXIT_FAILURE);
-		}
-	}
+    if (argc > 1) {
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1) {
+            fprintf(stderr, "Usage: %s <file>\n", argv[1]);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	if (fd != STDIN_FILENO)
-	{
-		fptr = fdopen(fd, "r");
-	}
-	else
-	{
-		fptr = stdin;
-	}
+    if (fd != STDIN_FILENO) {
+        fptr = fdopen(fd, "r");
+    } else {
+        fptr = stdin;
+    }
 
-	long int number;
+    long int number;
 
-	while ((nread = getline(&lineptr, &nread, fptr)) != -1)
-	{
-		number = strtol(lineptr, NULL, 10);
-		GetFactors(number);
-	}
+    while ((nread = getline(&lineptr, &bufsize, fptr)) != -1) {
+        number = strtol(lineptr, NULL, 10);
+        GetFactors(number);
+    }
 
-	free(lineptr);
-	fclose(fptr);
-	close(fd);
+    free(lineptr);
+    fclose(fptr);
+    close(fd);
 
-	return (0);
+    return 0;
 }
